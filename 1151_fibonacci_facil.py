09aa787814f66e2cee14798f6b56671767a2f2bd
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-07-15 12:22:54 -0300

    corrigido erro na resolução 1151

--- 1151_fibonacci_facil.py
+++ 1151_fibonacci_facil.py
@@ -1,13 +1,13 @@
 def fib(n):
 
-    if n == 0:
+    if n == 1:
         return 0
-    elif n == 1:
+    elif n == 2:
         return "0 1"
     else:
         lista_de_saida = ["0", "1"]
         a, b = 0, 1
-        for i in range(2, n + 1):
+        for i in range(2, n):
             a, b = b, a+b
             lista_de_saida.append(str(b))
         return " ".join(lista_de_saida)
